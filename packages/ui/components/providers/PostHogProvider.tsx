ac6069369ff94ff7a063e91d59d7e5f84ff666b1
enitrat <msaug@protonmail.com>
2025-02-24 20:21:25 +0100

    fix fmt

--- packages/ui/components/providers/PostHogProvider.tsx
+++ packages/ui/components/providers/PostHogProvider.tsx
@@ -3,30 +3,30 @@
 import { PostHogProvider as OriginalPostHogProvider } from 'posthog-js/react';
 
 export default function PostHogProviderClient({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-    const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
 
-    if (!posthogKey || !posthogHost) {
-        console.log('PostHog credentials missing, rendering without PostHog');
-        return <>{children}</>;
-    }
+  if (!posthogKey || !posthogHost) {
+    console.log('PostHog credentials missing, rendering without PostHog');
+    return <>{children}</>;
+  }
 
-    return (
-        <OriginalPostHogProvider
-            apiKey={posthogKey}
-            options={{
-                api_host: posthogHost,
-                session_recording: {
-                    recordCrossOriginIframes: true,
-                },
-                capture_pageleave: false,
-            }}
-        >
-            {children}
-        </OriginalPostHogProvider>
-    );
-} 
+  return (
+    <OriginalPostHogProvider
+      apiKey={posthogKey}
+      options={{
+        api_host: posthogHost,
+        session_recording: {
+          recordCrossOriginIframes: true,
+        },
+        capture_pageleave: false,
+      }}
+    >
+      {children}
+    </OriginalPostHogProvider>
+  );
+}
