adf1aa3fc86b423f2e11d54ffdb02fdc0fb50370
Fricoben <78437165+fricoben@users.noreply.github.com>
2025-02-24 19:24:20 +0000

    feat: add posthog analytics (#27)
    * feat: add posthog analytics
    
    * fix fmt
    
    ---------
    
    Co-authored-by: enitrat <msaug@protonmail.com>

---
+++ packages/ui/components/providers/PostHogProvider.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { PostHogProvider as OriginalPostHogProvider } from 'posthog-js/react';
+
+export default function PostHogProviderClient({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+  const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
+  if (!posthogKey || !posthogHost) {
+    console.log('PostHog credentials missing, rendering without PostHog');
+    return <>{children}</>;
+  }
+
+  return (
+    <OriginalPostHogProvider
+      apiKey={posthogKey}
+      options={{
+        api_host: posthogHost,
+        session_recording: {
+          recordCrossOriginIframes: true,
+        },
+        capture_pageleave: false,
+      }}
+    >
+      {children}
+    </OriginalPostHogProvider>
+  );
+}
